01a87ee399f7340e53a12554f21a7ef8f30f875f
JanTomassi <tomassijan@gmail.com>
2023-06-02 14:23:31 +0200

    02/06/2023 setup

--- statusbar.py
+++ statusbar.py
@@ -5,11 +5,10 @@ import threading
 
 prev_rx: int = 0
 prev_tx: int = 0
-is_locked: bool = False
 is_locking: int = 0
 widget_num: int = 0
 
-def run_on_interval_thread(t: int, f: callable, m: list = None, i: int = None):
+def run_on_interval_thread(t: int, f, m: list=None, i: int=None):
     e = 0
     while True:
         if (e % t) == 0:
@@ -21,7 +20,7 @@ def run_on_interval_thread(t: int, f: callable, m: list = None, i: int = None):
         time.sleep(1)
 
 
-def run_on_interval(t: int, f: callable, m: list = None):
+def run_on_interval(t: int, f, m = None):
     global widget_num
 
     if(m is not None):
@@ -66,15 +65,15 @@ def get_network():
 
 
 def get_date():
-    res: str = subprocess.run(["date", "+%c"], capture_output=True, text=True)
-    return res.stdout[:-1]
+    res: str = subprocess.run(["date", "+%c"], capture_output=True, text=True).stdout
+    return res[:-1]
 
 
 def get_package_update():
-    res_p: str = subprocess.run(["pacman", "-Quq"], capture_output=True, text=True)
-    res_y: str = subprocess.run(["yay", "-Quaq"], capture_output=True, text=True)
-    n_p = len(res_p.stdout.split("\n")[:-1])
-    n_y = len(res_y.stdout.split("\n")[:-1])
+    res_p: str = subprocess.run(["pacman", "-Quq"], capture_output=True, text=True).stdout
+    res_y: str = subprocess.run(["yay", "-Quaq"], capture_output=True, text=True).stdout
+    n_p = len(res_p.split("\n")[:-1])
+    n_y = len(res_y.split("\n")[:-1])
     return f"{n_p} + {n_y} 󰏔"
 
 
@@ -82,7 +81,7 @@ def get_free_space():
     res_space: str = subprocess.run(
         ["df", "-h", "/"], capture_output=True, text=True
     ).stdout
-    res_spaceL: str = res_space.split("\n")
+    res_spaceL: list[str] = res_space.split("\n")
     return res_spaceL[1].split()[4] + " 󰋊"
 
 
@@ -100,27 +99,23 @@ def unlock_if_near():
 
 
 def manage_bluetooth_locking():
-    global is_locked
     global is_locking
 
     is_device = get_bluetooth_device()
-    print("device %r\nlock %r" % (is_device, is_locked))
 
-    if is_device and is_locked:
+    if is_device:
         is_locking = 0
-        is_locked = False
-        unlock_if_near()
+        return " 󰄜"
 
-    elif not (is_device or is_locked) and (is_locking == 2):
-        is_locking = 0
-        is_locked = True
-
+    elif not (is_device) and (is_locking == 2):
+        is_locking += 1
         lock_if_no_dev()
+        return " 󰥐"
 
-    elif not (is_device or is_locked):
+    elif not (is_device) and (is_locking <= 2):
         is_locking += 1
         subprocess.run(["notify-send", "Phone not found for %d" % is_locking])
-
+        return " 󰥐"
 
 def update_root_window():
     global str_module
@@ -129,8 +124,7 @@ def update_root_window():
 
 
 def run_module():
-    run_on_interval(10, manage_bluetooth_locking)
-
+    run_on_interval(15, manage_bluetooth_locking, str_module)
     run_on_interval(1, get_network, str_module)
     run_on_interval(60 * 10, get_free_space, str_module)
     run_on_interval(60 * 60, get_package_update, str_module)
@@ -142,5 +136,5 @@ def run_module():
 if __name__ == "__main__":
     global str_module
 
-    str_module = [""]*4
+    str_module = [""]*5
     new_name = run_module()
