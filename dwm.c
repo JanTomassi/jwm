5f593187114416a7286e1e5478caca4bcf4bbb2a
Jan Tomassi <tomassijan@gmail.com>
2024-08-17 11:18:24 +0200

    feat: decrease layout symbol padding

--- dwm.c
+++ dwm.c
@@ -705,7 +705,7 @@ void drawbar(Monitor *m) {
     }
     w = TEXTW(m->ltsymbol);
     drw_setscheme(drw, scheme[SchemeSel]);
-    x = drw_text(drw, x, 0, w, bh, lrpad / 2, m->ltsymbol, 0);
+    x = drw_text(drw, x, 0, w - lrpad / 2, bh, lrpad / 4, m->ltsymbol, 0);
 
     if ((w = m->ww - tw - x) > bh) {
         if (m->sel) {
